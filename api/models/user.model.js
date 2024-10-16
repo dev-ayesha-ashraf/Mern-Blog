9d82b0b1a183c24d9790290bd8ab91f0eb1549bb
dev-ayesha-ashraf <ayeshaashraf0999@gmail.com>
2024-10-17 02:25:08 +0500

    usermodel Created

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+import { type } from "os";
+const userSchema = new mongoose.Schema({
+    username:{
+        type: 'string',
+        required: 'true',
+        unique: 'true'
+    },
+    username:{
+        type: 'string',
+        required: 'true',
+        unique: 'true'
+    },
+    password:{
+        type: 'string',
+        required: 'true',
+    },
+}, {timestamps: true}
+);
+
+const User = mongoose.model('User' , userSchema);
+export default User;
