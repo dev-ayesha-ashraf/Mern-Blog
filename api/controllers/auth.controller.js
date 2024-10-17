4b2c225cf1ae1f934a5189ae4a1713039b72244c
dev-ayesha-ashraf <ayeshaashraf0999@gmail.com>
2024-10-17 17:54:58 +0500

    signupAPIDone

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,20 +1,26 @@
 import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
+
 export const signup = async (req, res) => {
     const { username, email, password } = req.body;
-    if (!username || !email || !password || username === '' || email === '' || password === '') {
-        return res.status(400).json({ message: 'All Fields Are Required' })
-    }
 
-    const newUser = new User({
-        username,
-        email,
-        password
-    });
+    if (!username || !email || !password || username === '' || email === '' || password === '') {
+        return res.status(400).json({ message: 'All Fields Are Required' });
+    }
+const hashPassword = bcryptjs.hashSync(password, 10)
     try {
+        const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Username or Email already exists' });
+        }
+        const newUser = new User({
+            username,
+            email,
+            password: hashPassword,
+        });
         await newUser.save();
         res.json("Signup Successful");
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message });
     }
- 
 }
