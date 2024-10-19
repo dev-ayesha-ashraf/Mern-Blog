4f79db270b7f5fccd1ad883244ff03a7058a8b7f
dev-ayesha-ashraf <ayeshaashraf0999@gmail.com>
2024-10-19 20:59:45 +0500

    deleteAPI

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -2,7 +2,10 @@ import express from 'express';
 import { test } from '../controllers/user.controller.js';
 import { updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/vierifyUser.js';
+import {deleteUser} from '../controllers/user.controller.js'
 const router = express.Router();
 router.get('/test' , test);
-router.put('/update/:userId' ,verifyToken, updateUser)
+router.put('/update/:userId' ,verifyToken, updateUser);
+router.put('/delete/:userId' ,verifyToken, deleteUser);
+
 export default router;
