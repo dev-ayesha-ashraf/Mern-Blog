7cfb90b24bf3a9971716d7670acfb06aa7107cbe
dev-ayesha-ashraf <ayeshaashraf0999@gmail.com>
2024-10-30 18:49:31 +0500

    HomeUI

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -18,12 +18,34 @@
 
 //   )
 // }
-
+import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-// import CallToAction from '../components/CallToAction';
 import { useEffect, useState } from 'react';
 import PostCard from './Components/PostCard';
+import { FaTwitter, FaFacebook, FaArrowRight } from "react-icons/fa";
+const TypingText = ({ text, tag: Tag }) => {
+  const [displayedText, setDisplayedText] = useState('');
+  const typingSpeed = 90;
 
+  const typeText = (text) => {
+    let i = 0;
+    const interval = setInterval(() => {
+      if (i < text.length) {
+        setDisplayedText(text.slice(0, i + 1));
+        i++;
+      } else {
+        clearInterval(interval);
+      }
+    }, typingSpeed);
+  };
+
+  useEffect(() => {
+    typeText(text);
+  }, [text]);
+
+  const MotionTag = motion[Tag];
+  return <MotionTag className="text-5xl font-semibold text-indigo-900 text-center max-[570px]:text-4xl max-[420px]:text-2xl">{displayedText}</MotionTag>;
+};
 export default function Home() {
   const [posts, setPosts] = useState([]);
 
@@ -37,27 +59,29 @@ export default function Home() {
   }, []);
   return (
     <div>
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
-        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
-        <p className='text-gray-500 text-xs sm:text-sm'>
-          Here you'll find a variety of articles and tutorials on topics such as
-          web development, software engineering, and programming languages.
+      <div className='flex flex-col gap-6 pt-28 px-3 max-w-6xl mx-auto '>
+        <TypingText text="Welcome To Techie Blog" tag="h1" />
+        <p className='text-gray-500 text-xl text-center'>
+          Here you'll find a variety of articles and tutorials on Many topics.
         </p>
         <Link
-          to='/search'
-          className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
+          to={'/search'}
+          className="flex items-center justify-center text-center space-x-2 px-6 py-2 font-bold rounded-md shadow-lg transition duration-300 bg-[#85053a] text-white hover:opacity-90 cursor-pointer mx-auto"
         >
-          View all posts
+          <span>View all posts</span>
+          <FaArrowRight className="h-5 w-5" />
+
         </Link>
-      </div>
-      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
-        {/* <CallToAction /> */}
+
       </div>
 
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
         {posts && posts.length > 0 && (
-          <div className='flex flex-col gap-6'>
-            <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+          <div className='flex flex-col'>
+            <h2 className='text-3xl font-semibold text-center'>Recent Posts</h2>
+            <div className='w-full flex justify-center text-center items-center mt-2 mb-10'>
+              <span className='w-[100px] h-[3px] bg-[#85053a]'></span>
+            </div>
             <div className='flex flex-wrap gap-4'>
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
