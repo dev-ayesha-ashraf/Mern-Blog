83d803213c3b1c135eb5ae11fefa748d83b3e442
dev-ayesha-ashraf <ayeshaashraf0999@gmail.com>
2024-10-31 13:46:54 +0500

    PreInstallationAdded

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.routes.js'
 import postRoutes from './routes/post.route.js'
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from "cookie-parser";
+import path from 'path';
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("MongoDb Is Connected");
@@ -15,6 +16,9 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
     
 }
 )
+
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser())
@@ -27,6 +31,12 @@ app.use('/api/user' , userRoutes);
 app.use('/api/auth' , authRoutes);
 app.use('/api/post' , postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  });
 
 app.use((err, req,res,next) => {
     const statusCode = err.statusCode || 500;
